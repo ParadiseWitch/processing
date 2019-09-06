5e2605e3eda4d4ed5331bc3ca8ea4899747efb74
ParadiseWitch <zhp92@outlook.com>
2019-09-07 00:16:41 +0800

    Quadtree 2

--- p5.js/Quadtree/sketch.js
+++ p5.js/Quadtree/sketch.js
@@ -1,31 +1,44 @@
 let qtree;
 
+
 function setup() {
 	createCanvas(400,400);
 
+
 	let boundary = new Rectangle(200,200,200,200);
-
 	qtree = new Quadtree(boundary,4);
-	console.log(qtree);
+/*	console.log(qtree);*/
+	
+	// if(mouseIsPressed){
+		// 300个点
+		for(let i=0;i < 300; i++){
+			// let m = new Point(mouseX + random(-5,5),mouseY + random(-5,5));
+			let x = randomGaussian(width/2,width/8);
+			let y = randomGaussian(height/2,height/8);
+			let p = new Point(x,y);
+			qtree.insert(p);
+		}
+	// }
 
-	//在画布上随机 n 个点并插入到qt
-/*	for(let i=0;i<500;i++){
-		let p = new Point(random(width),random(height));
-		qt.insert(p);
-	}
 
-	background(0);
-	qt.show();*/
+
 }
 
 
 function draw() {
-	if(mouseIsPressed){
-		for(let i=0;i < 5; i++){
-			let m = new Point(mouseX + random(-5,5),mouseY + random(-5,5));
-			qtree.insert(m);
-		}
-	}
 	background(0);
 	qtree.show();
+
+	stroke(0,255,0);
+	rectMode(CENTER);
+	let range = new Rectangle(mouseX,mouseY,100,100);
+	rect(range.x,range.y,range.w*2,range.h*2);
+
+
+	let points = qtree.query(range);
+
+	for(let p of points){
+		strokeWeight(4);
+		point(p.x,p.y);
+	}
 }
